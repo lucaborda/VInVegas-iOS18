78ad75ab94386795f817956cfebcc33462426b78
luca borda <lucaborda11@gmail.com>
2024-07-23 14:07:52 -0300

    Initial Commit

---
+++ VacationInVegas/VacationInVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVegasApp.swift
+//  VacationInVegas
+//
+//  Created by Luca Borda on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct VacationInVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
